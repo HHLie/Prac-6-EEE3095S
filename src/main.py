ddecaba348a206f8654a73232b19f8ca5979efb2
LXXHSI007 <lxxhsi007@myuct.ac.za>
2021-10-31 14:21:46 +0200

    IDK
    dont know how to do the buttons

--- src/main.py
+++ src/main.py
@@ -1,9 +1,21 @@
 from flask import Flask
+from flask import request
+from flask import render_template
+from flask import redirect, url_for
+
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello EEE3095S! <br/> This is our webserver <br/> Done by: LXXHSI007 and VBNREE001'
+def index():
+    print(request.form)
+    if 'Sensor ON' in request.form:
+        print("on")
+        pass
+    if 'Sensor OFF' in request.form:
+        print("off")
+        pass
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=80)
