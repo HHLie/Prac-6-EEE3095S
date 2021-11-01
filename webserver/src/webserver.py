0eb195eba0e2b33bb926a18beb24396283a0ec09
LXXHSI007 <lxxhsi007@myuct.ac.za>
2021-11-01 21:27:51 +0200

    weserver tings

--- webserver/src/webserver.py
+++ webserver/src/webserver.py
@@ -1,4 +1,6 @@
 from flask import Flask, send_file, render_template
+import socket
+
 
 app = Flask(__name__)
 
@@ -6,22 +8,46 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/SensorStream')
-def sensor_stream():
+@app.route('/Logs_Check')
+def check():
     #TODO: Add code that displays the contents of log file /data/sensorlog.txt
-    print("sensor_stream")
+    print("check")
+
     return render_template('index.html')
+
 @app.route('/download')
 def download_file():
     print("download")
-    return render_template('index.html')
+    path = "/data/sensorlog.txt"
+    return send_file(path, as_attachment=True)
     #TODO: Add code to download the file /data.sensorlog.txt
 
 
 
 #TODO Add the remaining functions requested either by adding more pages to the template or get fancy with more templates and better formatting
+@app.route('/Sensor_ON')
+def sensor_on():
+    #TODO: Add code that displays the contents of log file /data/sensorlog.txt
+    print("sensor_on")
+    return render_template('index.html')
 
+@app.route('/Sensor_OFF')
+def sensor_off():
+    #TODO: Add code that displays the contents of log file /data/sensorlog.txt
+    print("sensor_off")
+    return render_template('index.html')
 
+@app.route('/Status')
+def status():
+    #TODO: Add code that displays the contents of log file /data/sensorlog.txt
+    print("status")
+    return render_template('index.html')
+
+@app.route('/Exit')
+def exit():
+    #TODO: Add code that displays the contents of log file /data/sensorlog.txt
+    print("exit")
+    return render_template('exit.html')
 
 
 if __name__ == '__main__':
